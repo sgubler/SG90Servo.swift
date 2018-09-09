d5c1c68417d285914f1d0738e43c2813f9e6b206
Samuel Gubler <sgubler@users.noreply.github.com>
2018-09-08 21:15:10 -0600

    Adjust duty cycle

--- Sources/SG90Servo.swift
+++ Sources/SG90Servo.swift
@@ -47,9 +47,12 @@ public class SG90Servo{
 }
 
 public enum Position: Int {
-    case left = 5
-    case middle = 8
-    case right = 12
+//     case left = 5
+//     case middle = 8
+//     case right = 12
+   case left = 1
+   case middle = 2
+   case right = 3
 }
 
 let SERVO_PERIOD_NS = 20_000_000  //20ms
