f8715a9c81c6c852f6c3c7a3b75172ff86c488ba
Samuel Gubler <sgubler@users.noreply.github.com>
2018-09-26 12:58:06 -0600

    Fix position values
    These were my findings of values for left right and middle positions.

--- Sources/SG90Servo.swift
+++ Sources/SG90Servo.swift
@@ -47,9 +47,9 @@ public class SG90Servo{
 }
 
 public enum Position: Int {
-    case left = 5
-    case middle = 8
-    case right = 12
+    case left = 1
+    case middle = 7
+    case right = 14
 }
 
-let SERVO_PERIOD_NS = 19_800_000  //19.8ms
+let SERVO_PERIOD_NS = 20_000_000  //20ms
